c0bd8d4c3681f80b51edbfc50bfdce8fcd47f3dc
James Schwan <james@schwan.us>
2022-05-13 11:21:52 -0500

    all

---
+++ uninstall.ps1
@@ -0,0 +1,35 @@
+# Set-ExecutionPolicy -ExecutionPolicy Unrestricted   --||--   Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+# UnInstall Monitoring
+
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    exit
+}
+
+$SCRIPT_PATH = split-path -parent $PSCommandPath
+Set-Location ${SCRIPT_PATH}
+
+$SETUP_FILE = "${SCRIPT_PATH}\setup.ps1"
+if (!(Test-Path $SETUP_FILE)) {
+    Write-Host "Setup file not found."
+    exit
+}
+. $SETUP_FILE
+
+if (Test-Path ${BASE_DIR}) {
+
+    Write-Host "Exit the OpenHardwareMonitor application now."
+    Pause
+
+    Set-Location ${BASE_DIR}
+    & "${BASE_DIR}\telegraf-${TELEGRAF_VER}\telegraf.exe" --service stop
+    & "${BASE_DIR}\telegraf-${TELEGRAF_VER}\telegraf.exe" --service uninstall
+
+    docker-compose down --volumes --rmi local
+
+    Remove-Item -Recurse -Force ${BASE_DIR}
+    Pause
+}
